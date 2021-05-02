220553cfff1b858bc44f106fc9ce2cde6327f286
jerry73204 <jerry73204@gmail.com>
2021-05-02 14:52:25 +0800

    Write doc comments

--- src/trait_.rs
+++ src/trait_.rs
@@ -2,23 +2,28 @@ use crate::{
     and_then::AndThen, map_err::MapErr, try_filter_map::TryFilterMap, try_flat_map::TryFlatMap,
 };
 
+/// An extension trait to [Iterator]
 pub trait IteratorExt {
+    /// Creates a fallible iterator that works like map, but flattens nested structure.
     fn try_flat_map<F, T, U, V, E>(self, f: F) -> TryFlatMap<Self, F, U::IntoIter>
     where
         Self: Sized + Iterator<Item = Result<T, E>>,
         F: FnMut(T) -> Result<U, E>,
         U: IntoIterator<Item = Result<V, E>>;
 
+    /// Creates a fallible iterator that both filters and maps.
     fn try_filter_map<F, T, U, E>(self, f: F) -> TryFilterMap<Self, F>
     where
         Self: Sized + Iterator<Item = Result<T, E>>,
         F: FnMut(T) -> Result<Option<U>, E>;
 
+    /// Maps a `Result<T, E>` to `Result<T, F>` by applying a function to a contained Err value, leaving an Ok value untouched.
     fn map_err<F, Ein, Eout>(self, f: F) -> MapErr<Self, F>
     where
         Self: Sized,
         F: FnMut(Ein) -> Eout;
 
+    /// Takes a closure and creates a fallible iterator which calls that closure on each element.
     fn and_then<F, T, U, E>(self, f: F) -> AndThen<Self, F>
     where
         Self: Sized + Iterator<Item = Result<T, E>>,
