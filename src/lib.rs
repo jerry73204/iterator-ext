5829f08d30ed9f507e6970c0bb4405ecd4f9bdea
jerry73204 <jerry73204@gmail.com>
2021-06-15 02:15:34 +0800

    Add try_filter() and try_flatten()

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,40 @@ mod and_then;
 mod common;
 mod map_err;
 mod trait_;
+mod try_filter;
 mod try_filter_map;
 mod try_flat_map;
+mod try_flatten;
 
 pub use and_then::*;
 pub use map_err::*;
 pub use trait_::*;
+pub use try_filter::*;
 pub use try_filter_map::*;
 pub use try_flat_map::*;
+pub use try_flatten::*;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn try_flatten_test_1() {
+        let input = vec![
+            Ok(vec![Ok(1usize), Ok(2)]),
+            Ok(vec![Err("err"), Ok(3)]),
+            Ok(vec![Ok(4)]),
+        ];
+        let output: Vec<_> = input.into_iter().try_flatten().collect();
+        assert_eq!(output, vec![Ok(1usize), Ok(2), Err("err")]);
+    }
+
+    #[test]
+    fn try_flatten_test_2() {
+        let input = vec![Ok(vec![Ok(1usize), Ok(2)]), Err("err"), Ok(vec![Ok(3)])];
+        let output: Vec<_> = input.into_iter().try_flatten().collect();
+        assert_eq!(output, vec![Ok(1usize), Ok(2), Err("err")]);
+    }
 
     #[test]
     fn try_flat_map_test_1() {
@@ -31,6 +53,16 @@ mod tests {
         let input = vec![Ok(vec![Ok(1usize), Ok(2)]), Err("err"), Ok(vec![Ok(3)])];
         let output: Vec<_> = input.into_iter().try_flat_map(Ok).collect();
         assert_eq!(output, vec![Ok(1usize), Ok(2), Err("err")]);
+    }
+
+    #[test]
+    fn try_filter_test() {
+        let input = vec![Ok(1usize), Ok(2), Ok(3), Err("err"), Ok(4)];
+        let output: Vec<_> = input
+            .into_iter()
+            .try_filter(|val| Ok(val % 2 == 1))
+            .collect();
+        assert_eq!(output, vec![Ok(1usize), Ok(3), Err("err")]);
     }
 
     #[test]
--- src/trait_.rs
+++ src/trait_.rs
@@ -1,39 +1,11 @@
 use crate::{
-    and_then::AndThen, map_err::MapErr, try_filter_map::TryFilterMap, try_flat_map::TryFlatMap,
+    and_then::AndThen, map_err::MapErr, try_filter::TryFilter, try_filter_map::TryFilterMap,
+    try_flat_map::TryFlatMap, try_flatten::TryFlatten,
 };
 
 /// An extension trait to [Iterator]
 pub trait IteratorExt {
     /// Creates a fallible iterator that works like map, but flattens nested structure.
-    fn try_flat_map<F, T, U, V, E>(self, f: F) -> TryFlatMap<Self, F, U::IntoIter>
-    where
-        Self: Sized + Iterator<Item = Result<T, E>>,
-        F: FnMut(T) -> Result<U, E>,
-        U: IntoIterator<Item = Result<V, E>>;
-
-    /// Creates a fallible iterator that both filters and maps.
-    fn try_filter_map<F, T, U, E>(self, f: F) -> TryFilterMap<Self, F>
-    where
-        Self: Sized + Iterator<Item = Result<T, E>>,
-        F: FnMut(T) -> Result<Option<U>, E>;
-
-    /// Maps a `Result<T, E>` to `Result<T, F>` by applying a function to a contained Err value, leaving an Ok value untouched.
-    fn map_err<F, Ein, Eout>(self, f: F) -> MapErr<Self, F>
-    where
-        Self: Sized,
-        F: FnMut(Ein) -> Eout;
-
-    /// Takes a closure and creates a fallible iterator which calls that closure on each element.
-    fn and_then<F, T, U, E>(self, f: F) -> AndThen<Self, F>
-    where
-        Self: Sized + Iterator<Item = Result<T, E>>,
-        F: FnMut(T) -> Result<U, E>;
-}
-
-impl<I> IteratorExt for I
-where
-    I: Iterator,
-{
     fn try_flat_map<F, T, U, V, E>(self, f: F) -> TryFlatMap<Self, F, U::IntoIter>
     where
         Self: Sized + Iterator<Item = Result<T, E>>,
@@ -47,6 +19,31 @@ where
         }
     }
 
+    /// Creates a fallible iterator that flattens nested structure.
+    fn try_flatten<T, U, E>(self) -> TryFlatten<Self, T::IntoIter>
+    where
+        Self: Sized + Iterator<Item = Result<T, E>>,
+        T: IntoIterator<Item = Result<U, E>>,
+    {
+        TryFlatten {
+            iter: Some(self),
+            sub_iter: None,
+        }
+    }
+
+    /// Creates a fallible iterator that filters items.
+    fn try_filter<F, T, E>(self, f: F) -> TryFilter<Self, F>
+    where
+        Self: Sized + Iterator<Item = Result<T, E>>,
+        F: FnMut(&T) -> Result<bool, E>,
+    {
+        TryFilter {
+            iter: Some(self),
+            f,
+        }
+    }
+
+    /// Creates a fallible iterator that both filters and maps.
     fn try_filter_map<F, T, U, E>(self, f: F) -> TryFilterMap<Self, F>
     where
         Self: Sized + Iterator<Item = Result<T, E>>,
@@ -58,6 +55,7 @@ where
         }
     }
 
+    /// Maps a `Result<T, E>` to `Result<T, F>` by applying a function to a contained Err value, leaving an Ok value untouched.
     fn map_err<F, Ein, Eout>(self, f: F) -> MapErr<Self, F>
     where
         Self: Sized,
@@ -66,6 +64,7 @@ where
         MapErr { iter: self, f }
     }
 
+    /// Takes a closure and creates a fallible iterator which calls that closure on each element.
     fn and_then<F, T, U, E>(self, f: F) -> AndThen<Self, F>
     where
         Self: Sized + Iterator<Item = Result<T, E>>,
@@ -77,3 +76,5 @@ where
         }
     }
 }
+
+impl<I> IteratorExt for I where I: Iterator {}
